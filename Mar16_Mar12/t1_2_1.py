db8b3a5e516221ee587409fe34ad6781db652f05
Your Name <you@example.com>
2026-03-16 12:45:51 +0200

    v1

--- Mar16_Mar12/t1_2_1.py
+++ Mar16_Mar12/t1_2_1.py
@@ -1,12 +1,14 @@
 # a * x**2 + b * x + c = 0
 # a, b, c
 
-#def solve_quadr_eq(a, b, c):
 
-s = '   hello world a b c 1222  \n '
-print(f'strip: |{s.strip()}|')
-print(f'strip: |{s.rstrip()}|')
 
 with open('input01.txt') as f:
     for line in f:
-        print(line.rstrip())
+        s = line.strip()
+        if len(s) == 0:
+            continue
+        d = s.split()
+        coefs = list(map(int, d))
+        a, b, c = coefs
+        print(a, b, c)
