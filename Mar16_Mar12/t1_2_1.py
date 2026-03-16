f3ad1135750cdf1b44c6ec448e88990de1cfe382
Your Name <you@example.com>
2026-03-16 12:56:36 +0200

    v1a

--- Mar16_Mar12/t1_2_1.py
+++ Mar16_Mar12/t1_2_1.py
@@ -1,6 +1,63 @@
 # a * x**2 + b * x + c = 0
 # a, b, c
 
+def show_quadr_eq(a, b, c):
+    print('Рівняння з коефіцієнтами:', a, b, c)
+
+def solve_quadr_eq(a, b, c):
+    if a != 0 :  # або abs(a)<1e-12
+        D = b**2 - 4 * a * c
+        if D < 0:
+            print("Розв'язків немає")
+        elif D == 0: # або abs(D)<1e-12
+            x0 = -b/(2*a)
+            print("Розв'язок:", x0)
+        else:
+            x1 = (-b - D**0.5)/(2*a)
+            x2 = (-b + D**0.5)/(2*a)
+            print("Розв'язки:", x1, x2)
+        #
+    else:
+        print('Це не квадратне рівняння')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 with open('input01.txt') as f:
@@ -11,4 +68,5 @@ with open('input01.txt') as f:
         d = s.split()
         coefs = list(map(int, d))
         a, b, c = coefs
-        print(a, b, c)
+        show_quadr_eq(a, b, c)
+        solve_quadr_eq(a, b, c)
