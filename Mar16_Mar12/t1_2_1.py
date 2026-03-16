101350a349dde7d0eae7ee97f4c622f7225b2e9c
Your Name <you@example.com>
2026-03-16 13:53:56 +0200

    v2a

--- Mar16_Mar12/t1_2_1.py
+++ Mar16_Mar12/t1_2_1.py
@@ -1,40 +1,56 @@
 # a * x**2 + b * x + c = 0
 # a, b, c
-# {'coef_a': ...., 'coef_b': ..., 'coef_c': ...}
 
 class QuadraticEquation:
-    def __init__(self, a, b, c, line_no):
-        self.coef_a = a
-        self.coef_b = b
-        self.coef_c = c
-        self.line_no = line_no
+    def __init__(self, data, line_no):
+        if isinstance(data, str): # чи містить змінна data щось, що ми звикли називати "текстовий рядок" ?
+            print('нам передали в змінній data текстовий рядок')
+            d = data.split()
+            cfs = list(map(int, d))
+            self.coef_a = cfs[0]
+            self.coef_b = cfs[1]
+            self.coef_c = cfs[2]
+            self.line_no = line_no
+        elif isinstance(data, list): # чи містить змінна data щось, що ми звикли називати "список" ?
+            print('нам передали в змінній data текстовий список')
+            self.coef_a = data[0]
+            self.coef_b = data[1]
+            self.coef_c = data[2]
+            self.line_no = line_no
+        elif isinstance(data, QuadraticEquation): # чи містить змінна data щось, що є вже кимось раніше створеним квадратним рівнянням?
+            self.coef_a = data.coef_a
+            self.coef_b = data.coef_b
+            self.coef_c = data.coef_c
+            self.line_no = data.line_no
+        else:
+            print('нам передали в змінній data щось, з чим ми не вміємо працювати!')
+            raise ValueError
+
 
     def show_quadr_eq(self):
-        a = self.coef_a
-        b = self.coef_b
-        c = self.coef_c
-        line_no = self.line_no
-        print('Рівняння з коефіцієнтами:', a, b, c, 'із рядка №', line_no)
+        print('Рівняння з коефіцієнтами:', self.coef_a, self.coef_b, self.coef_c,
+              'із рядка №', self.line_no)
 
     def solve_quadr_eq(self):
-        a = self.coef_a
-        b = self.coef_b
-        c = self.coef_c
-
-        if a != 0 :  # або abs(a)<1e-12
-            D = b**2 - 4 * a * c
+        if self.coef_a != 0 :  # або abs(a)<1e-12
+            D = self.coef_b**2 - 4 * self.coef_a * self.coef_c
             if D < 0:
                 print("Розв'язків немає")
+                return []
             elif D == 0: # або abs(D)<1e-12
-                x0 = -b/(2*a)
+                x0 = -self.coef_b/(2*self.coef_a)
                 print("Розв'язок:", x0)
+                return [x0]
             else:
-                x1 = (-b - D**0.5)/(2*a)
-                x2 = (-b + D**0.5)/(2*a)
+                x1 = (-self.coef_b - D**0.5)/(2*self.coef_a)
+                x2 = (-self.coef_b + D**0.5)/(2*self.coef_a)
                 print("Розв'язки:", x1, x2)
+                return [x1, x2]
             #
         else:
             print('Це не квадратне рівняння')
+            return None
+
 
 
 
@@ -42,11 +58,10 @@ with open('input01.txt') as f:
     j = 0
     for line in f:
         s = line.strip()
-        j += 1
         if len(s) == 0:
             continue
-        d = s.split()
-        cfs = list(map(int, d))
-        e = QuadraticEquation(cfs[0], cfs[1], cfs[2], j)
+        j += 1
+
+        e = QuadraticEquation(line, j)
         e.show_quadr_eq()
         e.solve_quadr_eq()
