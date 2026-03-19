657f4447d6f316f484a87a5e2df8ed5ae4ffe4e6
timn <i-wont-tell@you.com>
2026-03-19 11:27:24 +0200

    v2

--- Mar19/t_2_2.py
+++ Mar19/t_2_2.py
@@ -1,9 +1,25 @@
 import turtle
 
-turtle.speed(10)
+turtle.speed(1)
 
-turtle.goto(100, 100)
-turtle.goto(-100, 200)
+x0, y0 = (-100, -50)
+x1, y1 = 80, 50
+x2, y2 = 180, -150
+
+turtle.penup()
+turtle.goto(x0, y0)
+turtle.pendown()
+
+turtle.color('red')
+
+turtle.fillcolor('yellow')
+turtle.begin_fill()
+
+turtle.goto(x0+x1, y0+y1)
+turtle.goto(x0+x2, y0+y2)
+turtle.goto(x0, y0)
+
+turtle.end_fill()
 
 
 turtle.mainloop()
