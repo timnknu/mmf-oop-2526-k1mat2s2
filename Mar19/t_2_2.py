5ae8b7df5ba84d0fc305d296d14d7596506224c6
timn <i-wont-tell@you.com>
2026-03-19 11:05:35 +0200

    v1a

--- Mar19/t_2_2.py
+++ Mar19/t_2_2.py
@@ -1,12 +1,9 @@
 import turtle
 
-turtle.forward(100)
+turtle.speed(10)
 
-turtle.setheading(45)
-turtle.forward(150)
-
-turtle.setheading(135)
-turtle.forward(150)
+turtle.goto(100, 100)
+turtle.goto(-100, 200)
 
 
 turtle.mainloop()
