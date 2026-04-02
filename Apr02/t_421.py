d1821b5f2fb73ca3ce31c15060fbf16e8dbdf082
Your Name <you@example.com>
2026-04-02 11:52:10 +0300

    v2

--- Apr02/t_421.py
+++ Apr02/t_421.py
@@ -7,27 +7,34 @@ class Figure:
         self._color = 'red'
     def set_color(self, clr):
         self._color = clr
+    def _draw(self):
+        raise NotImplementedError("Потрібно спершу створити клас-нащадок")
+    def show(self):
+        turtle.color(self._color)
+        self._draw()
+    def hide(self):
+        turtle.color('white')
+        self._draw()
 
 class Circle(Figure):
     def __init__(self, x0, y0, r):
         super().__init__(x0, y0)
         self._r = r
-    def draw(self):
+    def _draw(self):
         turtle.penup()
         turtle.goto(self._x0, self._y0 - self._r)
         turtle.pendown()
-        turtle.color(self._color)
         turtle.circle(self._r)
+
 
 class Square(Figure):
     def __init__(self, x0, y0, side):
         super().__init__(x0, y0)
         self._a = side
-    def draw(self):
+    def _draw(self):
         turtle.penup()
         turtle.goto(self._x0, self._y0)
         turtle.pendown()
-        turtle.color(self._color)
         turtle.goto(self._x0, self._y0 + self._a)
         turtle.goto(self._x0 + self._a, self._y0 + self._a)
         turtle.goto(self._x0 + self._a, self._y0)
@@ -36,11 +43,17 @@ class Square(Figure):
 
 turtle.speed(1)
 
+# f = Figure(100, 50)
+# f.show()
+
 s = Square(100, 50, 50)
 s.set_color('blue')
-s.draw()
+s.show()
+s.hide()
 
-# c = Circle(-150, 80, 70)
-# c.draw()
+c = Circle(-150, 80, 70)
+c.show()
+c.hide()
+c.show()
 
 turtle.mainloop()
